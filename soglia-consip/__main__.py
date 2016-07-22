3b43ecd3e6c18e7f0225a1659c52f3490a2c6320
Massimo Santini <massimo.santini@gmail.com>
2016-07-23 00:16:15 +0200

    New

---
+++ soglia-consip/__main__.py
@@ -0,0 +1,24 @@
+from urllib import urlopen
+from re import sub
+
+from bs4 import BeautifulSoup
+
+URL = 'https://www.timinternet.it/timmobile/private/wp.do'
+
+def clean(e):
+	return sub( r'<[^>]+>', '', u' '.join(map(lambda _: unicode(_).strip(), e.contents)))
+
+data = urlopen( URL ).read()
+soup = BeautifulSoup(data, 'html.parser')
+
+info, _, traffico = soup.find_all('table')[:3]
+
+linea, contratto = info.find_all('tr')[:2]
+print u'\t'.join( map( clean, linea.find_all('td') ) )
+print u'\t'.join( map( clean, contratto.find_all('td') ) )
+
+tipo, dettagli, residui, totali, scandenza = traffico.find_all('tr')[1].find_all('td')
+print 'Tipo', clean(tipo)
+print 'Aggiornato il', clean(dettagli)
+print 'Residui', u' '.join(clean(residui).split())
+print 'Totali', u' '.join(clean(totali).split())
